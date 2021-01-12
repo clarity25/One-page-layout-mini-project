ea3108e23e5cf462d0f72159ffbcf916f4ef351e
Reyan John Narvaja <33762901+clarity25@users.noreply.github.com>
2021-01-12 21:06:44 +0800

    minor Fixing

--- js/parallax.js
+++ js/parallax.js
@@ -3,7 +3,7 @@ const parallax = document.getElementById('home');
 window.addEventListener('scroll', function(){
     let offset = window.pageYOffset;
     console.log(offset);
-    parallax.style.backgroundPositionY = offset / 5 + "px";    
+    parallax.style.backgroundPositionY = offset / 2 + "px";    
 
     navbarScrollEffect(offset);
     
@@ -19,32 +19,39 @@ const navbarScrollEffect = (offset) =>{
     // if(offset > 400 && offset < 1000){
     //     li[1].style.backgroundColor = 'red';
     // }
+    let navBarScrollBackgrounColor = 'aquamarine';
+    let navbarColor = '#604C68';
+    let navBarScrollBackgrounColorNoBackgroundImage = '#ffff';
 
 
-    if(offset < 100){
+    let navbar = document.querySelector('header');
+
+    if(offset < 600){
         var home = document.querySelector('.li:nth-Child(1)');
         var dogo = document.querySelector('.li:nth-Child(2)');
         home.style.backgroundColor = ' aquamarine';
         dogo.style.backgroundColor = 'transparent';
+        navbar.style.backgroundColor = 'transparent';
     }else if(offset > 400 && offset < 1000){
         var home = document.querySelector('.li:nth-Child(1)');
         var dogo = document.querySelector('.li:nth-Child(2)');
         var shimonada = document.querySelector('.li:nth-Child(3)');
         home.style.backgroundColor = 'transparent';
-        dogo.style.backgroundColor = 'red';
+        dogo.style.backgroundColor = navBarScrollBackgrounColorNoBackgroundImage;
         shimonada.style.backgroundColor = 'transparent';
+        navbar.style.backgroundColor = navbarColor;
     }else if(offset > 1000 && offset < 1550){
         var dogo = document.querySelector('.li:nth-Child(2)');
         var shimonada = document.querySelector('.li:nth-Child(3)');
         var minobu = document.querySelector('.li:nth-Child(4)');
         dogo.style.backgroundColor = 'transparent';
-        shimonada.style.backgroundColor = 'red';
+        shimonada.style.backgroundColor = navBarScrollBackgrounColor;
         minobu.style.backgroundColor = 'transparent';
     }else if(offset > 1550 && offset < 2150){
         var minobu = document.querySelector('.li:nth-Child(4)');
         var shimonada = document.querySelector('.li:nth-Child(3)');
         var yakushima = document.querySelector('.li:nth-Child(5)');
-        minobu.style.backgroundColor = 'red';
+        minobu.style.backgroundColor = navBarScrollBackgrounColor;
         shimonada.style.backgroundColor = 'transparent';
         yakushima.style.backgroundColor = 'transparent';
     }else if(offset > 2150 && offset < 2840){
@@ -52,48 +59,48 @@ const navbarScrollEffect = (offset) =>{
         var yakushima = document.querySelector('.li:nth-Child(5)');
         var ikebukuro = document.querySelector('.li:nth-Child(6)');
         minobu.style.backgroundColor = 'transparent';
-        yakushima.style.backgroundColor = 'red';
+        yakushima.style.backgroundColor = navBarScrollBackgrounColor;
         ikebukuro.style.backgroundColor = 'transparent';
     }else if(offset > 2840 && offset < 3430){
         var yakushima = document.querySelector('.li:nth-Child(5)');
         var ikebukuro = document.querySelector('.li:nth-Child(6)');
         var washinomiya = document.querySelector('.li:nth-Child(7)');
         yakushima.style.backgroundColor = 'transparent';
-        ikebukuro.style.backgroundColor = 'red';
+        ikebukuro.style.backgroundColor = navBarScrollBackgrounColor;
         washinomiya.style.backgroundColor = 'transparent';
     }else if(offset > 3430 && offset < 4060){
         var ikebukuro = document.querySelector('.li:nth-Child(6)');
         var washinomiya = document.querySelector('.li:nth-Child(7)');
         var iwami = document.querySelector('.li:nth-Child(8)');
         ikebukuro.style.backgroundColor = 'transparent';
-        washinomiya.style.backgroundColor = 'red';
+        washinomiya.style.backgroundColor = navBarScrollBackgrounColor;
         iwami.style.backgroundColor = 'transparent';
     }else if(offset > 4060 && offset < 4683){
         var washinomiya = document.querySelector('.li:nth-Child(7)');
         var iwami = document.querySelector('.li:nth-Child(8)');
         var toyosato = document.querySelector('.li:nth-Child(9)');
         washinomiya.style.backgroundColor = 'transparent';
-        iwami.style.backgroundColor = 'red';
+        iwami.style.backgroundColor = navBarScrollBackgrounColor;
         toyosato.style.backgroundColor = 'transparent';
     }else if(offset > 4683 && offset < 5300){
         var iwami = document.querySelector('.li:nth-Child(8)');
         var toyosato = document.querySelector('.li:nth-Child(9)');
         var kaikan = document.querySelector('.li:nth-Child(10)');
         iwami.style.backgroundColor = 'transparent';
-        toyosato.style.backgroundColor = 'red';
+        toyosato.style.backgroundColor = navBarScrollBackgrounColor;
         kaikan.style.backgroundColor = 'transparent';
     }else if(offset > 5300 && offset < 6000){
         var toyosato = document.querySelector('.li:nth-Child(9)');
         var kaikan = document.querySelector('.li:nth-Child(10)');
         var ogimachi = document.querySelector('.li:nth-Child(11)');
         toyosato.style.backgroundColor = 'transparent';
-        kaikan.style.backgroundColor = 'red';
+        kaikan.style.backgroundColor = navBarScrollBackgrounColor;
         ogimachi.style.backgroundColor = 'transparent';
     }else if(offset > 6000 && offset < 6500){
         var kaikan = document.querySelector('.li:nth-Child(10)');
         var ogimachi = document.querySelector('.li:nth-Child(11)');
         kaikan.style.backgroundColor = 'transparent';
-        ogimachi.style.backgroundColor = 'red';
+        ogimachi.style.backgroundColor = navBarScrollBackgrounColor;
 
     }else if(offset > 6500 && offset < 6888){
         var kaikan = document.querySelector('.li:nth-Child(10)');
